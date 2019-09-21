3d56b8332c17178e7ab661a3d5b712d2968c6b71
Tianze <bgcsoft@hotmail.com>
2019-09-21 22:10:23 +0800

    update

--- public/js/page_list.js
+++ public/js/page_list.js
@@ -8,8 +8,6 @@ app.controller('controller', function($scope, $http) {
     var type=localStorage.getItem("type");
     var typee=type;
 
-    $("#typeTitle")[0].innerHTML=typee;
-
     switch (type) {
         case "Q1文章数量":
             type="Q1";
@@ -25,16 +23,92 @@ app.controller('controller', function($scope, $http) {
             break
     }
 
-    var url="http://127.0.0.1/shinfo/public/api/output/lists/"+type+"/"+updateDate+"/"+university+"/"+dicipline+"/all/10";
+    var limit=30;
+    var offset=0;
+    var pageNum=1;
+    var url="http://127.0.0.1/shinfo/public/api/output/lists/"+type+"/"+updateDate+"/"+university+"/"+dicipline+"/all/"+limit+"/"+offset;
+    console.log(url);
     $scope.getList=function(){
         $http.get(url)
             .success(function (response) {
-                console.log(response);
+                // console.log(response);
                 $scope.list=[];
+                $scope.count=response.count;
+                $("#typeTitle")[0].innerHTML=typee+"  机构: "+university+"  共"+$scope.count+"条";
                 for(var i=0;i<response.list.length;i++){
                     $scope.list.push(response.list[i]);
                 }
+                $scope.page=Math.ceil($scope.count/limit);
+                console.log("共 "+$scope.page+"页");
             });
     }
     $scope.getList();
+
+     $scope.pageTurning=function(e){
+         var lastPage=parseInt($("#lastPage")[0].innerHTML);
+         if(e=="next"){
+             offset+=limit;
+             pageNum++;
+         }
+         else if(e=="pre"){
+             offset-=limit;
+             pageNum--;
+         }
+         else if(e=="end"){
+             offset=limit*(lastPage-1);
+             pageNum=lastPage;
+         }else{
+             offset=limit*(e-1);
+             pageNum=e;
+         }
+         url="http://127.0.0.1/shinfo/public/api/output/lists/"+type+"/"+updateDate+"/"+university+"/"+dicipline+"/all/"+limit+"/"+offset;
+         console.log(url);
+         $scope.getList();
+         console.log("页码："+pageNum);
+         $scope.changePageList();
+    }
+
+    $scope.changePageList=function(){
+         if(pageNum==1){
+             $("#prePage").addClass("disabled");
+         }else{
+             $("#prePage").removeClass("disabled");
+         }
+         if(pageNum==parseInt($("#lastPage")[0].innerHTML)){
+             $("#nextPage").addClass("disabled");
+             $("#lastPage").parent().addClass("active");
+         }else{
+             $("#nextPage").removeClass("disabled");
+         }
+
+         var pId="#p"+pageNum;
+         if($(pId)[0]==undefined){
+             var f=$("#p li").eq(0).children()[0].innerHTML;
+            if(parseInt(pageNum)>parseInt(f)){
+                var f4=parseInt(f)+4;
+                var f5=parseInt(f)+5;
+                var f6=parseInt(f)+6;
+                var f7=parseInt(f)+7;
+                $("#p")[0].innerHTML="<li class=\"page-item active\" id=\"p"+f4+"\"><a class=\"page-link\" href=\"#\" ng-click=\"pageTurning("+f4+")\">"+f4+"</a></li>\n" +
+                    "                        <li class=\"page-item\" id=\"p"+f5+"\"><a class=\"page-link\" href=\"#\" ng-click=\"pageTurning("+f5+")\">"+f5+"</a></li>\n" +
+                    "                        <li class=\"page-item\" id=\"p"+f6+"\"><a class=\"page-link\" href=\"#\" ng-click=\"pageTurning("+f6+")\">"+f6+"</a></li>\n" +
+                    "                        <li class=\"page-item\" id=\"p"+f7+"\"><a class=\"page-link\" href=\"#\" ng-click=\"pageTurning("+f7+")\">"+f7+"</a></li>";
+            }else{
+                var f=$("#p li").eq(3).children()[0].innerHTML;
+                var f4=parseInt(f)-7;
+                var f5=parseInt(f)-6;
+                var f6=parseInt(f)-5;
+                var f7=parseInt(f)-4;
+                $("#p")[0].innerHTML="<li class=\"page-item\" id=\"p"+f4+"\"><a class=\"page-link\" href=\"#\" ng-click=\"pageTurning("+f4+")\">"+f4+"</a></li>\n" +
+                    "                        <li class=\"page-item\" id=\"p"+f5+"\"><a class=\"page-link\" href=\"#\" ng-click=\"pageTurning("+f5+")\">"+f5+"</a></li>\n" +
+                    "                        <li class=\"page-item\" id=\"p"+f6+"\"><a class=\"page-link\" href=\"#\" ng-click=\"pageTurning("+f6+")\">"+f6+"</a></li>\n" +
+                    "                        <li class=\"page-item active\" id=\"p"+f7+"\"><a class=\"page-link\" href=\"#\" ng-click=\"pageTurning("+f7+")\">"+f7+"</a></li>";
+            }
+         }else{
+             $(pId).siblings().removeClass("active");
+             $(pId).addClass("active");
+         }
+
+    }
+
 });
--- resources/views/pages/list.blade.php
+++ resources/views/pages/list.blade.php
@@ -2,8 +2,8 @@
 @section("content")
     <div class="row mt-2">
         <div class="col-10 m-auto pt-2">
-            <h4 id="typeTitle"></h4>
-            <div class="list-group mt-1">
+            <h4 id="typeTitle" class="mt-2"></h4>
+            <div class="list-group mt-2">
                 <div class="list-group-item list-group-item-action flex-column align-items-start" ng-repeat="x in list">
                     <div class="d-flex justify-content-between align-items-center">
                         <p>@{{x.paperTitle}}</p>
@@ -12,6 +12,25 @@
                     <small><strong>机构：</strong>@{{x.dis_uni_name}} &nbsp;<strong>出版年份：</strong>@{{x.pubYear}} &nbsp;<strong>ID:</strong> @{{x.paperID}}</small>
                 </div>
             </div>
+
+            <nav aria-label="Page navigation example" class="mt-3">
+                <ul class="pagination justify-content-center">
+                    <li class="page-item disabled" id="prePage">
+                        <a class="page-link" href="#" tabindex="-1" ng-click="pageTurning('pre')">上一页</a>
+                    </li>
+                    <ul class="pagination" id="p">
+                        <li class="page-item active" id="p1"><a class="page-link" href="#" ng-click="pageTurning(1)">1</a></li>
+                        <li class="page-item" id="p2"><a class="page-link" href="#" ng-click="pageTurning(2)">2</a></li>
+                        <li class="page-item" id="p3"><a class="page-link" href="#" ng-click="pageTurning(3)">3</a></li>
+                        <li class="page-item" id="p4"><a class="page-link" href="#" ng-click="pageTurning(4)">4</a></li>
+                    </ul>
+                    <li class="page-item disabled"><a class="page-link" href="#">...</a></li>
+                    <li class="page-item"><a class="page-link" id="lastPage" href="#" ng-click="pageTurning('end')">@{{page}}</a></li>
+                    <li class="page-item" id="nextPage">
+                        <a class="page-link" href="#" ng-click="pageTurning('next')">下一页</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </div>
     <script src="{{URL::asset('js/page_list.js')}}"></script>
